afe0796c518de541ff7db79e35d19c03f7468f37
Andrew <andrii.fedotiuk@bonkers.ie>
2022-06-07 11:20:03 +0100

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+enum EStoreTypes {
+    TEST = 'TEST',
+}
---
+++ src/test/test.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia';
+import EStoreTypes from '../constants';
+
+const useTestStore = defineStore(EStoreTypes.TEST, {
+    state: () => {
+        return {
+            counter: 0,
+            name: 'Eduardo',
+            isAdmin: true,
+        }
+    },
+    getters: {
+        doubleCount: (state) => state.counter * 2,
+    },
+    actions: {
+        increment() {
+            this.counter++
+        },
+        randomizeCounter() {
+            this.counter = Math.round(100 * Math.random())
+        },
+    },
+})

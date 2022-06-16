e7fbf613c13056e87cfd02a783ff08b3a29f5dfb
Andrew <andrii.fedotiuk@bonkers.ie>
2022-06-16 15:55:10 +0100

    update(*): update config + add HomePage

--- src/example/example.ts
+++ src/example/example.ts
@@ -2,13 +2,11 @@ import { defineStore } from "pinia";
 import { EStoreTypes } from "../constants";
 
 export const useTestStore = defineStore(EStoreTypes.TEST, {
-	state: () => {
-		return {
+	state: () => ({
 			counter: 0,
 			name: "Eduardo",
 			isAdmin: true,
-		}
-	},
+		}),
 	getters: {
 		getCounter: (state) => state.counter,
 		getDoubleCount: (state) => state.counter * 2,
